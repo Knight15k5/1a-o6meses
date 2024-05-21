db5481d58f3c52cc3ace4f3d45ae9a53e9294e7d
Miguel Alonso Clavo Bardales <105908010+Knight15k5@users.noreply.github.com>
2024-05-21 00:30:17 -0500

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,10 @@
+const words = ['Eres mi lugar favorito para escapar del mundo','Contigo todos los dias son especiales','Si tu fueras una galleta serias la mas dulce del tarro',
+'A tu lado cualquier lugar es mi hogar','Eres la melodia que siempre suena en mi corazon','Contigo cada momento se convierte en un recuerdo inolvidable',
+'Eres mi sol en los dias grises y mi estrella en las noches oscuras','Eres tan especial que haces que mis malos chistes suenen bien',
+'Eres como una pizza caliente deliciosa y siempre quiero mas','No estoy seguro de que hice para merecer a alguien tan increible como tu pero no pienso cuestionarlo',
+'Dicen que el amor es ciego pero contigo veo el mundo con colores mas brillantes y un poco borroso a veces xd',
+'Eres como un buen vino cuanto mas tiempo paso contigo mejor me siento','Cada beso tuyo es una estrella que ilumina mi cielo nocturno',
+'Contigo hasta las discusiones mas absurdas se convierten en aventuras dignas de recordar','Juntos somos dos almas que bailan en la misma melodia sin importar las notas altas o bajas',
+'Contigo descubri que el amor es el arte de encontrar la belleza en los detalles mas pequeños de la vida',
+'Eres como el cafe me mantienes despierto, feliz y un poco adicto','Tu cariño es el ingrediente secreto que hace que mi vida tenga sabor'
+];
